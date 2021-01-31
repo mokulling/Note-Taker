e9d2ddceee427046382864a4da7b5a9a1dd857ef
mokulling <72229978+mokulling@users.noreply.github.com>
2021-01-31 16:11:22 -0600

    skeleton of express server

---
+++ server.js
@@ -0,0 +1,50 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var db = require("./db/db.json")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+
+// Create New Characters - takes in JSON input
+app.post("/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
+  console.log(newNote);
+
+  db.push(newNote);
+
+  res.json(newNote);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
